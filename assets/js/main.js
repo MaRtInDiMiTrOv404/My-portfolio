6da4155eebb00bd457935b39edbdb14166f56027
MaRtInDiMiTrOv404 <168213887+MaRtInDiMiTrOv404@users.noreply.github.com>
2024-12-29 23:17:15 +0200

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,8 +46,8 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2000,
-    delay: 200,
+    duration: 600,
+    delay: 100,
 //     reset: true
 });
 
